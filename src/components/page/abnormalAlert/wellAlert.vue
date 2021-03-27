7f81efa7725866d93d5c6e6b2b9f83ef2db10fcf
任天然 <74280134+Jakewil@users.noreply.github.com>
2021-03-27 12:05:48 +0800

    Update wellAlert.vue

--- src/components/page/abnormalAlert/wellAlert.vue
+++ src/components/page/abnormalAlert/wellAlert.vue
@@ -46,9 +46,7 @@
                             <span style="color: #FFFFFF;margin: 5px;">油井现场图</span>
                         <img src="../../../assets/img/hangzhoumap.png" style="width:680px;height: 450px;" />
                         </el-col>
-<!--                        <el-col :span="6" style="margin: 10px;">-->
-<!--                        <img src="../../../assets/img/alert.png" style="width:550px;height: 300px;left: 300px;top:21px; position: relative;" />-->
-<!--                        </el-col>-->
+
 
                         <el-col :span="6" style="margin: 10px;">
                         <span style="color: #FFFFFF;margin: 5px;left: 400px;position: relative">异常数据可视化</span>
@@ -60,7 +58,6 @@
                         </el-col>
                     </el-row>
                     <br>
-<!--                    <br>-->
                     <br>
                     <el-row style='top:-65px'>
                         <el-col :span="12" style='left:80px;position: relative'>
@@ -730,103 +727,7 @@
                     ]
                 }
                 mychart.setOption(option)
-            },
-            // loadxinjiang() {
-            //     var ROOT_PATH = 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples';
-            //
-            //     var chartDom = document.getElementById('xinjiang');
-            //     var myChart = echarts.init(chartDom);
-            //     var option;
-            //
-            //     myChart.showLoading();
-            //     $.get(ROOT_PATH + '../../../assets/mapjson/xianggang.json', function(geoJson) {
-            //
-            //         myChart.hideLoading();
-            //
-            //         echarts.registerMap('HK', geoJson);
-            //
-            //         myChart.setOption(option = {
-            //             title: {
-            //                 text: '',
-            //             },
-            //             tooltip: {
-            //                 trigger: 'item',
-            //                 formatter: '{b}<br/>{c} (p / km2)'
-            //             },
-            //             toolbox: {
-            //                 show: true,
-            //                 orient: 'vertical',
-            //                 left: 'right',
-            //                 top: 'center',
-            //                 feature: {
-            //                     dataView: { readOnly: false },
-            //                     restore: {},
-            //                     saveAsImage: {}
-            //                 }
-            //             },
-            //             visualMap: {
-            //                 min: 800,
-            //                 max: 50000,
-            //                 text: ['High', 'Low'],
-            //                 realtime: false,
-            //                 calculable: true,
-            //                 inRange: {
-            //                     color: ['lightskyblue', 'yellow', 'orangered']
-            //                 }
-            //             },
-            //             series: [
-            //                 {
-            //                     name: '香港18区人口密度',
-            //                     type: 'map',
-            //                     mapType: 'HK', // 自定义扩展图表类型
-            //                     label: {
-            //                         show: true
-            //                     },
-            //                     data: [
-            //                         { name: '中西区', value: 20057.34 },
-            //                         { name: '湾仔', value: 15477.48 },
-            //                         { name: '东区', value: 31686.1 },
-            //                         { name: '南区', value: 6992.6 },
-            //                         { name: '油尖旺', value: 44045.49 },
-            //                         { name: '深水埗', value: 40689.64 },
-            //                         { name: '九龙城', value: 37659.78 },
-            //                         { name: '黄大仙', value: 45180.97 },
-            //                         { name: '观塘', value: 55204.26 },
-            //                         { name: '葵青', value: 21900.9 },
-            //                         { name: '荃湾', value: 4918.26 },
-            //                         { name: '屯门', value: 5881.84 },
-            //                         { name: '元朗', value: 4178.01 },
-            //                         { name: '北区', value: 2227.92 },
-            //                         { name: '大埔', value: 2180.98 },
-            //                         { name: '沙田', value: 9172.94 },
-            //                         { name: '西贡', value: 3368 },
-            //                         { name: '离岛', value: 806.98 }
-            //                     ],
-            //                     // 自定义名称映射
-            //                     nameMap: {
-            //                         'Central and Western': '中西区',
-            //                         'Eastern': '东区',
-            //                         'Islands': '离岛',
-            //                         'Kowloon City': '九龙城',
-            //                         'Kwai Tsing': '葵青',
-            //                         'Kwun Tong': '观塘',
-            //                         'North': '北区',
-            //                         'Sai Kung': '西贡',
-            //                         'Sha Tin': '沙田',
-            //                         'Sham Shui Po': '深水埗',
-            //                         'Southern': '南区',
-            //                         'Tai Po': '大埔',
-            //                         'Tsuen Wan': '荃湾',
-            //                         'Tuen Mun': '屯门',
-            //                         'Wan Chai': '湾仔',
-            //                         'Wong Tai Sin': '黄大仙',
-            //                         'Yau Tsim Mong': '油尖旺',
-            //                         'Yuen Long': '元朗'
-            //                     }
-            //                 }
-            //             ]
-            //         });
-            //     });
+            },         
             }
     }
 
